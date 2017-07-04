2fa430e5f9ed94725aac204d593ec2ad25aa92e1
Bill Nguyen <dtn2114@columbia.edu>
2017-07-04 13:48:58 -0400

    Let's Start done

---
+++ let-start/hello.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+int main()
+{
+  std::cout << "Hello World" std::endl;
+  return 0;
+}
+
+// std (short form of standard): a namespace and cout is defined in this namespace
+// a namespace is a special area inside which something is defined.
+// std::cout is used to use the definition of cout from std namespace
+
+// << is the output operator
+
+/* std::cout is an object from iostream library
+ #include link the program to the iostream library
+*/
+
+/* #include <iostream> will make std::cout available for use
+ std::cout << "Hello World" will use the definition of cout from std namespace
+*/
+
+/*
+int main() - main is the name of the function, int will return an integer output
+all C++ programs must have this main() function, it is executed first
+*/
+
+
+
