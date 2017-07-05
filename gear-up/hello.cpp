beed2338aaed67f844e8db746c11b57a719b0f52
Bill Nguyen <dtn2114@columbia.edu>
2017-07-05 01:49:31 -0400

    gear-up

--- gear-up/hello.cpp
+++ gear-up/hello.cpp
@@ -1,28 +1,23 @@
 #include <iostream>
 int main()
 {
-  std::cout << "Hello World" std::endl;
+  int num;
+  num = 2;
+  std::cout << num;
+  using  std::cout;
+  cout << "Hello World" << std::endl;
+  cout << "This is my second line" << std::endl;
   return 0;
 }
 
-// std (short form of standard): a namespace and cout is defined in this namespace
-// a namespace is a special area inside which something is defined.
-// std::cout is used to use the definition of cout from std namespace
-
-// << is the output operator
-
-/* std::cout is an object from iostream library
- #include link the program to the iostream library
-*/
-
-/* #include <iostream> will make std::cout available for use
- std::cout << "Hello World" will use the definition of cout from std namespace
-*/
+/*
+Whitespace: tabs, spaces, newlines
+   */
 
 /*
-int main() - main is the name of the function, int will return an integer output
-all C++ programs must have this main() function, it is executed first
-*/
-
-
-
+Identifiers: names of any user-defined elements
+int num: declare var num is an int
+num = 2: assigning 2 to 'num'. So the value of 'num' is 2.
+Here, num is the name of a var and thus is an identifier.
+C++ is a case-sensitive language.
+   */
