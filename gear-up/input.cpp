fb7c5abc91e74a62e2386dd0ae0b173010addcf7
Bill Nguyen <dtn2114@columbia.edu>
2017-07-20 01:30:07 -0400

    finish gear-up

--- gear-up/input.cpp
+++ gear-up/input.cpp
@@ -7,3 +7,17 @@ int main()
   return 0;
 }
 
+
+
+/*
+Conventions:
+- Identifiers cannot be given the name of a keyword
+- Identifiers can consist of only letters, digits, and underscore
+- Identifiers can only start with a letter or an underscore (not digits).
+- It is a general convention to name methods & functions with camelCase
+starting with lower case. e.g. getMarks(), setAge()
+- The braces enclosing the body of the function should be aligned
+with the functinon name.
+- Within the braces, each statement should start from a distance
+of one tab from the alignment of the braces.
+ */
